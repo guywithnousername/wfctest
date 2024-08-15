350848762f1d9853a65970761343bb2651982823
Minglang Du <minglangdu@gmail.com>
2024-08-14 17:14:33 -0700

    finish making function

--- main.cpp
+++ main.cpp
@@ -195,16 +195,19 @@ int main() {
             
         }
     }
+    cout << "A\n";
     vector<vector<char>> ans = generate(grid, n2, m2);
-    for (int i = 0; i < n2; i ++) {
-        for (int j = 0; j < m2; j ++) {
-            cout << "\033[0m";
-            if (ans[i][j] == '*') cout << "\033[33m";
-            if (ans[i][j] == '~') cout << "\033[34m";
-            if (ans[i][j] == '#') cout << "\033[1;32m";
-            cout << ans[i][j];
+    for (int it = 0; it < nums; it ++) {
+        for (int i = 0; i < n2; i ++) {
+            for (int j = 0; j < m2; j ++) {
+                cout << "\033[0m";
+                if (ans[i][j] == '*') cout << "\033[33m";
+                if (ans[i][j] == '~') cout << "\033[34m";
+                if (ans[i][j] == '#') cout << "\033[1;32m";
+                cout << ans[i][j];
+            }
+            cout << "\n";
         }
-        cout << "\n";
+        cout << "----\n";
     }
-    cout << "----\n";
 }
